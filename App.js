a846963f33eb4bed72b0e4c20aa7a78a669f07ab
Mustafa <mjebbar17@gmail.com>
2017-12-26 23:55:01 +0000

    indentation and status bar height

--- App.js
+++ App.js
@@ -1,43 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+const statusBarHeight = Platform.OS === 'android' ? StatusBar.currentHeight: 20;
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.buttons}>
-            <Text style={styles.buttonText}>Search</Text>
-        </View>
-        <View style={styles.buttons}>
-            <Text style={styles.buttonText}>Photos</Text>
-        </View>
-        <View style={styles.buttons}>
-            <Text style={styles.buttonText}>Tags</Text>
-        </View>
-      </View>
-    );
-  }
+
+    render() {
+        return (
+           <View style={styles.container}>
+               <View style={styles.buttons}>
+                   <Text style={styles.buttonText}>Search</Text>
+               </View>
+               <View style={styles.buttons}>
+                   <Text style={styles.buttonText}>Photos</Text>
+               </View>
+               <View style={styles.buttons}>
+                   <Text style={styles.buttonText}>Tags</Text>
+               </View>
+           </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttons: {
-      backgroundColor: '#1a1a1a',
-      flexGrow: 1,
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderWidth: 1,
-      borderStyle: 'solid',
-      borderColor: '#9f9f9f',
-  },
-  buttonText:{
+    container: {
+        marginTop: statusBarHeight,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttons: {
+        backgroundColor: '#1a1a1a',
+        flexGrow: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#9f9f9f',
+    },
+    buttonText:{
         color: 'whitesmoke',
         fontSize: 20,
-  }
+    }
 });
