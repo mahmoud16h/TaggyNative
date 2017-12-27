7652deb2982540fdbee287114ec8f57bd77a2cd2
Mustafa <mjebbar17@gmail.com>
2017-12-27 22:56:16 +0000

    modal in progress

--- screens/photoScreen.js
+++ screens/photoScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, StatusBar, Dimensions, ScrollView, CameraRoll, Image } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar, Dimensions, ScrollView, CameraRoll, Image, TouchableHighlight, Modal } from 'react-native';
 import {Permissions } from 'expo';
 const statusBarHeight = Platform.OS === 'android' ? StatusBar.currentHeight: 20;
 const screenWidth = Dimensions.get('window').width;
@@ -10,7 +10,9 @@ class PhotoScreen extends React.Component {
         super(props);
         this.state = {
             hasCameraPermission: false,
-            photos : []
+            photos : [],
+            showModal : false,
+            selectedPhoto: null
         }
     }
 
@@ -32,27 +34,60 @@ class PhotoScreen extends React.Component {
            });
     }
 
-    render() {
+    handleImageClick = (photo) => {
+        console.log(photo);
+        this.setState({selectedPhoto : photo, showModal: true});
+    }
 
+    closeModal = () => {
+        this.setState({showModal : false})
+    }
+
+    getModalContent = () => {
+        if(!this.state.showModal) return null;
+        let photo = this.state.selectedPhoto;
+        let uri = photo.node.image.uri;
+        let isLandscape =  photo.node.image.width > photo.node.image.height;
+        let alpha = screenWidth/photo.node.image.width;
+        return(<View style={styles.modal}>
+            <Image
+               style={{
+                   width: screenWidth,
+                   height: photo.node.image.height * alpha
+               }}
+               source={{uri}}/>
+        </View>)
+    }
+
+    render() {
         if (this.state.hasCameraPermission === false) {
             return <Text>No access to camera</Text>;
         } else {
             return (
                <View style={styles.container}>
+                   {this.state.showModal &&
+                   <Modal isVisible={this.state.showModal} onRequestClose={this.closeModal}>
+                       {this.getModalContent()}
+                   </Modal>
+                   }
+
                    <Text>Photos</Text>
                    <ScrollView>
                        <View style={styles.imageHolder}>
-                           {this.state.photos.map((p, i) => {
-                               let uri = p.node.image.uri;
+                           {this.state.photos.map((photo, i) => {
+                               let uri = photo.node.image.uri;
                                let imageWidth = screenWidth / 3;
-                               return (<Image
+                               return (<TouchableHighlight
                                      key={i}
-                                     style={{
-                                         width: imageWidth,
-                                         height: imageWidth,
-                                     }}
-                                     source={{uri}}
-                                  />
+                                     onPress={() => this.handleImageClick(photo)}>
+                                      <Image
+                                         style={{
+                                             width: imageWidth,
+                                             height: imageWidth,
+                                         }}
+                                         source={{uri}}
+                                      />
+                                  </TouchableHighlight>
                                );
                            })}
                        </View>
@@ -75,5 +110,10 @@ const styles = StyleSheet.create({
     imageHolder: {
         flexDirection: 'row',
         flexWrap: 'wrap'
+    },
+    modal : {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 });
